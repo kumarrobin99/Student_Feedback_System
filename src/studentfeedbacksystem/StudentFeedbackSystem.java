de5f4e940f88d4b5c03e536a3b59205cfd3f7b7b
Robin Kumar <68186209+kumarrobin99@users.noreply.github.com>
2020-10-14 17:41:32 +0530

    Add files via upload

---
+++ src/studentfeedbacksystem/StudentFeedbackSystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentfeedbacksystem;
+
+/**
+ *
+ * @author kumar
+ */
+public class StudentFeedbackSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Welcome nw=new Welcome();
+        nw.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
